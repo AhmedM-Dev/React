a2ad3ce03c2b440d0668a85508dc64ec9172161f
Ahmed Mahmoudi <PROXYM-IT\ahmed.mahmoudi@prx-lp0083.proxym-it.tn>
2019-08-27 15:33:10 +0100

    Added stupid content

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>
-            Hello I a testing React
+            Hello I am testing React, I am new here so be kind please.
           </p>
           <a
             className="App-link"
